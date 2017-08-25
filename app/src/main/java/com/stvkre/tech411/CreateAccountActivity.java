5b93609655e384c696edea722aa86d1eff4d2caa
stvkre <kiariemburuh@gmail.com>
2017-08-25 12:42:02 +0300

    CreateUserAccount -Created new user on firebase

--- app/src/main/java/com/stvkre/tech411/CreateAccountActivity.java
+++ app/src/main/java/com/stvkre/tech411/CreateAccountActivity.java
@@ -1,13 +1,89 @@
 package com.stvkre.tech411;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+
+public static final String TAG = CreateAccountActivity.class.getSimpleName();
+
+    @Bind(R.id.createUserButton) Button mCreateUserButton;
+    @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mLoginTextView.setOnClickListener(this);
+        mCreateUserButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        // navigate to the login page if user already has account
+
+        if (view == mLoginTextView) {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+
+            // flags to handle tasks within the activities while navigating
+
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+        if (view == mCreateUserButton) {
+            createNewUser();
+        }
+
+    }
+
+    // createNewUser method
+
+    private void createNewUser() {
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        // create new user account on Firebase
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "Authentication successful");
+                } else {
+                    Toast.makeText(CreateAccountActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
