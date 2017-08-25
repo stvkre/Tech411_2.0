e9298b9590d731aa8bf2b042e2ea85c41b44ee05
stvkre <kiariemburuh@gmail.com>
2017-08-25 11:23:40 +0300

    added login page

---
+++ app/src/main/java/com/stvkre/tech411/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.stvkre.tech411;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
