bb881d479913e11a139c2fef8b3e869a9ffe054b
stvkre <kiariemburuh@gmail.com>
2017-08-25 16:40:03 +0300

    RecyclerView -set variables for recyclerview objects

--- app/src/main/java/com/stvkre/tech411/MainActivity.java
+++ app/src/main/java/com/stvkre/tech411/MainActivity.java
@@ -2,12 +2,20 @@ package com.stvkre.tech411;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mBlogList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBlogList = (RecyclerView) findViewById(R.id.blog_list);
+        mBlogList.setHasFixedSize(true);
+        mBlogList.setLayoutManager(new LinearLayoutManager(this));
     }
 }
